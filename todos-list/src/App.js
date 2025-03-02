957262792fb0fc095aa930695a4e6521aadcc2a9
DhiaShams <dhiadhilu123@gmail.com>
2025-03-02 09:08:59 +0530

    Modified Header.js App.js

--- todos-list/src/App.js
+++ todos-list/src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
 
 function App() {
+  let todos=[
+    {
+      sno:1,
+      title:"todos list project",
+      desc:"Lets complete this..Woohoo!!"
+    },
+    {
+      sno:2,
+      title:"OS Assignment",
+      desc:"PPT pending..."
+    },
+    {
+      sno:1,
+      title:"Ritm",
+      desc:"Lyric extraction from audio.. download not done!"
+    }
+  ]
   return (
     <>
-      <Header/>
+      <Header title="My Todos List" searchBar={false}/>
+      <Todos todos={todos}/>
+      <Footer/>
     </>
   );
 }
